b70f34c9f184ddaf8eea3e82d8a3446805f7f5a0
Melissa S Armstrong <armstrong@zoology.ubc.ca>
2016-12-04 21:07:24 -0800

    import stats package for qdiff

--- R/qdiff.R
+++ R/qdiff.R
@@ -14,6 +14,6 @@
 #' #' qdiff(z, probs = c(0.25, 0.75), type = 1)
 qdiff <- function(x, probs = c(0, 1), na.rm = TRUE, ...) {
   stopifnot(is.numeric(x))
-  the_quantiles <- quantile(x = x, probs = probs, na.rm = na.rm, ...)
+  the_quantiles <- stats::quantile(x = x, probs = probs, na.rm = na.rm, ...)
   return(max(the_quantiles) - min(the_quantiles))
 }
