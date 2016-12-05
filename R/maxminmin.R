5f4ca42cb258cfc13c3cbcfa5e3899b6f619b69b
Melissa S Armstrong <armstrong@zoology.ubc.ca>
2016-12-04 20:50:06 -0800

    added description, license and documentation for both functions

--- R/maxminmin.R
+++ R/maxminmin.R
@@ -1,3 +1,14 @@
+#' Max - Min
+#'
+#' Combines baseR functions of max - min. Later will build into another, more generalized function...
+#'
+#' @param x numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' #' max_min_min(iris$Sepal.Length), max_min_min(iris$Petal.Length)
 max_min_min <- function(x) {
   if(!is.numeric(x)) {
     stop('I am so sorry, but this function only works for numeric input!\n',
--- R/qdiff.R
+++ R/qdiff.R
@@ -1,4 +1,19 @@
+#' Quantile - Quantile
+#'
+#' @param x numeric
+#' @param probs the quantiles of interest
+#' @param na.rm TRUE or FALSE: what do you want to do with NAs?
+#' @param ... 9 different algorithms for computing quantiles
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' #' set.seed(1234)
+#' #' z <- rnorm(10)
+#' #' qdiff(z, probs = c(0.25, 0.75), type = 1)
 qdiff <- function(x, probs = c(0, 1), na.rm = TRUE, ...) {
+  stopifnot(is.numeric(x))
   the_quantiles <- quantile(x = x, probs = probs, na.rm = na.rm, ...)
   return(max(the_quantiles) - min(the_quantiles))
 }
