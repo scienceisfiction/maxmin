36a5e7b9152bed7a80d2f484f0a4bee3e74ed02f
Melissa S Armstrong <armstrong@zoology.ubc.ca>
2016-12-04 21:13:27 -0800

    added package level documentation

---
+++ R/maxmin-package.r
@@ -0,0 +1,9 @@
+#' maxmin: find the diff in quantiles
+#'
+#' A crazy simple use of baseR max and min to find the difference. Then a slightly more interesting generalized version that can take any quantile of interest to the user.
+#' qdiff includes a couple different arguments including na.rm and control of which type of algorithm generates the quantile.
+#'
+#'
+#' @name maxmin
+#' @docType package
+NULL
