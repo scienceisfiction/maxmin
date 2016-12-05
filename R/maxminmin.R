8cadd6d6cd93a5592c7c60455a2094cbbbc41a70
Melissa S Armstrong <armstrong@zoology.ubc.ca>
2016-12-04 20:25:41 -0800

    added assertions to maxminmin, added qdiff function

--- R/maxminmin.R
+++ R/maxminmin.R
@@ -1,1 +1,7 @@
-max_min_min <- function(x) max(x) - min(x)
+max_min_min <- function(x) {
+  if(!is.numeric(x)) {
+    stop('I am so sorry, but this function only works for numeric input!\n',
+         'You have provided an object of class: ', class(x)[1])
+  }
+  max(x) - min(x)
+}
---
+++ R/qdiff.R
@@ -0,0 +1,4 @@
+qdiff <- function(x, probs = c(0, 1), na.rm = TRUE, ...) {
+  the_quantiles <- quantile(x = x, probs = probs, na.rm = na.rm, ...)
+  return(max(the_quantiles) - min(the_quantiles))
+}
